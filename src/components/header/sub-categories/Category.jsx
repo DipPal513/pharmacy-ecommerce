e0637dec247e736b6814708a030df83d71119ee6
dippal513 <dip.pal.513@gmail.com>
2025-03-17 22:42:41 +0600

    color changed

--- src/components/header/sub-categories/Category.jsx
+++ src/components/header/sub-categories/Category.jsx
@@ -22,7 +22,7 @@ export default function Category() {
       <div className="relative max-w-screen-xl mx-auto">
         <select className="w-fit px-4 py-2 border border-gray-300 rounded bg-white text-gray-700 focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none">
           {categories.map((category, index) => (
-            <option key={index} value={category.name} className="p-2 text-gray-900">
+            <option key={index} value={category.name} className="p-2 text-gray-500">
               {category.name}
             </option>
           ))}
